a405aacfd594476eb23432d3c003b8441466e45c
albert <albert@mobidoo.co.kr>
2020-10-29 23:31:36 +0900

    init commit

---
+++ app/src/main/java/kr/co/weather/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package kr.co.weather.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kr.co.weather.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
